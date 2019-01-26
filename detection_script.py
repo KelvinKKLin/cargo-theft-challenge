3b829e9bd88d98bdd76fea98eb4a6c2512ea645e
Kelvin Lin <kelvinkklin@gmail.com>
2019-01-26 15:05:25 -0500

    Fixed processed data

--- detection_script.py
+++ detection_script.py
@@ -5,6 +5,7 @@ import random
 class DataPoint():
 	def __init__(self, date, latitude, longitude):
 		self.date = date
+		self.time = time
 		self.latitude = latitude
 		self.longitude = longitude
 
@@ -18,18 +19,11 @@ longitude_tolerance = [10, 10, 10, 10, 10, 10, 10]
 time_tolerance = [10, 10, 10, 10, 10, 10, 10]
 
 # Load the data.	
-processed_data = open("processed_data.csv", "r")
+processed_data = open("processed_data2.csv", "r")
 for line in processed_data.readlines()[1:]:
 	line = line.split(",")
-	if("NULL" not in line):
-		data = DataPoint(datetime.strptime(line[0], "%M:%S.%f").time(), float(line[1]), float(line[2]))
+	if "NULL" not in line:
+		datetime_string = line[0]+ " " + line[1]
+		data = DataPoint(datetime.strptime(datetime_string, "%Y-%m-%d %I:%M:%S %p"), float(line[2]), float(line[3]))
 		data_set.append(data)
 processed_data.close()
-
-# Generaate the random number.
-def generateRandomNumber():
-	return random.randint(100000000000, 9999999999999)
-
-# Check if 2 numbers are the same.
-def checkNumber(num1, num2):
-	return num1 == num2
