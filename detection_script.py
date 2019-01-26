facb8c88f783de6ef0300d7128961cdf06ed1b4f
Kelvin Lin <kelvinkklin@gmail.com>
2019-01-26 15:38:45 -0500

    Add check algorithm

--- detection_script.py
+++ detection_script.py
@@ -1,25 +1,21 @@
-from datetime import datetime, time
+from datetime import datetime
 import random
 
 # Wrapper class for the data.
 class DataPoint():
 	def __init__(self, date, latitude, longitude):
 		self.date = date
-		self.time = time
 		self.latitude = latitude
 		self.longitude = longitude
 
 		
 # Variables.
 data_set = []
-last_latitude = 0
-last_longitude = 0
 latitude_tolerance = [10, 10, 10, 10, 10, 10, 10]
 longitude_tolerance = [10, 10, 10, 10, 10, 10, 10]
-time_tolerance = [10, 10, 10, 10, 10, 10, 10]
 
 # Load the data.	
-processed_data = open("processed_data2.csv", "r")
+processed_data = open("processed_data.csv", "r")
 for line in processed_data.readlines()[1:]:
 	line = line.split(",")
 	if "NULL" not in line:
@@ -27,3 +23,32 @@ for line in processed_data.readlines()[1:]:
 		data = DataPoint(datetime.strptime(datetime_string, "%Y-%m-%d %I:%M:%S %p"), float(line[2]), float(line[3]))
 		data_set.append(data)
 processed_data.close()
+
+def IsHijackInProgress(latitude, longitude):
+	# Get the current date and day of the week.
+	current_date = datetime.today()
+	current_weekday = 3 #current_date.weekday()
+	
+	# Get parameters based on day of the week.
+	lat_tol = latitude_tolerance[current_weekday]
+	long_tol = longitude_tolerance[current_weekday]
+		
+	# Get all datapoints captured on the same date.
+	for datapoint in data_set:
+		if datapoint.date.weekday() == current_weekday:
+			# Calcualte tolerance ranges.
+			latitude_high = datapoint.latitude + lat_tol
+			latitude_low = datapoint.latitude - lat_tol
+			longitude_high = datapoint.longitude + long_tol
+			longitude_low = datapoint.longitude - long_tol
+			
+			print(latitude, longitude)
+			print(latitude_high, latitude_low)
+			print(longitude_high, longitude_low)
+			
+			# Determine if current coordinates are within range.
+			if (latitude < latitude_high) and (latitude > latitude_low) and (longitude < longitude_high) and (longitude > longitude_low):
+				return False
+	
+	# Return if hijack is in progress
+	return True
